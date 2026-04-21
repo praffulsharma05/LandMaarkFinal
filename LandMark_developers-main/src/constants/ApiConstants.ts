215012ae5ac4f7047674c7e0e8d92f344c061835
Prafful Sharma <sharmaprafful646@gmail.com>
2026-04-21 12:17:57 +0530

    temp

--- LandMark_developers-main/src/constants/ApiConstants.ts
+++ LandMark_developers-main/src/constants/ApiConstants.ts
@@ -1,4 +1,6 @@
 export const ApiConstants = {
     //API_BASE_URL: "http://localhost:5000/"
-    API_BASE_URL:"https://biotic-shasta-undeliberative.ngrok-free.dev/"
+    API_BASE_URL:"https://biotic-shasta-undeliberative.ngrok-free.dev/",
+    // API_BASE_URL:"https://landmaarkdeveloper.com",
+    UNKNOWN: "unknown"
 }
