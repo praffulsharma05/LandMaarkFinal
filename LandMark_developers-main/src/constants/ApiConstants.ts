c43aa5e11eeb048badac09e90bf5613fa0f09fff
Prafful Sharma <sharmaprafful646@gmail.com>
2026-04-22 11:12:09 +0530

    contact us page make a responsive

--- LandMark_developers-main/src/constants/ApiConstants.ts
+++ LandMark_developers-main/src/constants/ApiConstants.ts
@@ -1,7 +1,7 @@
 export const ApiConstants = {
     //API_BASE_URL: "http://localhost:5000/"
-    // API_BASE_URL:"https://unimmunized-rosella-hedonistically.ngrok-free.dev/",
-    API_BASE_URL:"http://localhost:3000/",
+    API_BASE_URL:"https://unimmunized-rosella-hedonistically.ngrok-free.dev/",
+    // API_BASE_URL:"http://localhost:3000/",
     UNKNOWN: "unknown",
     HEADERS: {
         'Cache-Control': 'no-cache',
